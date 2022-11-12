78b61d99f1eaee00f6c6ceb5df120c4a827171c9
Rohan Singh R <rohansingh8861@gmail.com>
2022-11-12 17:51:38 +0530

    10th commit

---
+++ trenwow/lib/Signer/companygst.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:trenwow/splasher/companygstSplash.dart';
+
+class CompanyGST extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Verification',
+          style: TextStyle(
+              fontSize: 25,
+              color: Colors.amber[400],
+              fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 5,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/trenwow.png'),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'GST',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.greenAccent[400]),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 40,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.height / 3,
+            child: TextField(
+              keyboardType: TextInputType.number,
+              maxLength: 15,
+              decoration: InputDecoration(
+                hintText: 'Enter company\'s GST number',
+                labelText: 'GST',
+                hintStyle: TextStyle(color: Colors.grey),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange)),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => CompanyGSTSplash()));
+                    },
+                    child: Text("Next ->")),
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
