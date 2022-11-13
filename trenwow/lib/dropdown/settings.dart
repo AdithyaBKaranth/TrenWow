1b6d60fa5cd9f9357e6623b7d51a7010ce7ba26b
Rohan Singh R <rohansingh8861@gmail.com>
2022-11-13 08:50:45 +0530

    15th commit

--- trenwow/lib/dropdown/settings.dart
+++ trenwow/lib/dropdown/settings.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:trenwow/dropdown/account.dart';
+import 'package:trenwow/dropdown/appLang.dart';
+import 'package:trenwow/dropdown/chat.dart';
 import 'package:trenwow/dropdown/prof.dart';
+import 'package:trenwow/dropdown/purchaseHis.dart';
 
 class Settings extends StatelessWidget {
   @override
@@ -67,62 +70,80 @@ class Settings extends StatelessWidget {
           SizedBox(
             height: MediaQuery.of(context).size.height / 30,
           ),
-          ListTile(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            leading: Icon(
-              Icons.message,
-              color: Colors.black,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Chat()));
+            },
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              leading: Icon(
+                Icons.message,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Chats',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text(
+                'Theme,wallpapers,chat history',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              tileColor: Colors.purple[300],
             ),
-            title: Text(
-              'Chats',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-            ),
-            subtitle: Text(
-              'Theme,wallpapers,chat history',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            tileColor: Colors.purple[300],
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height / 30,
           ),
-          ListTile(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            leading: Icon(
-              Icons.language,
-              color: Colors.black,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => AppLang()));
+            },
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              leading: Icon(
+                Icons.language,
+                color: Colors.black,
+              ),
+              title: Text(
+                'App language',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text(
+                'English',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              tileColor: Colors.purple[300],
             ),
-            title: Text(
-              'App language',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-            ),
-            subtitle: Text(
-              'English',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            tileColor: Colors.purple[300],
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height / 30,
           ),
-          ListTile(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            leading: Icon(
-              Icons.sell,
-              color: Colors.black,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PurchaseHis()));
+            },
+            child: ListTile(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              leading: Icon(
+                Icons.sell,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Purchase history',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              subtitle: Text(
+                'Purchase details',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              tileColor: Colors.purple[300],
             ),
-            title: Text(
-              'Purchase history',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-            ),
-            subtitle: Text(
-              'Purchase details',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            tileColor: Colors.purple[300],
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height / 30,
